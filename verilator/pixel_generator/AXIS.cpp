b11beb8ac9ea5b90e6d081c8512d75d779c550be
Kevin Huang <kevinhuang997@gmail.com>
2025-05-20 22:33:45 +0100

    Finished AXIS tb seems ok

--- verilator/pixel_generator/AXIS.cpp
+++ verilator/pixel_generator/AXIS.cpp
@@ -6,10 +6,10 @@
 #include <cstdlib>
 #include <ctime>
 
-#define X_SIZE 480       // X dimension of image in words (words = pixels * 3/4)
-#define Y_SIZE 480       // Y dimension of image
-#define ENDTIME 10000000 // End time of simulation
-#define TIMEOUT 1000     // Time to wait for valid to be true
+#define X_SIZE 480     // X dimension of image in words (words = pixels * 3/4)
+#define Y_SIZE 480     // Y dimension of image
+#define ENDTIME 500000 // End time of simulation, giving 500000 clock cycles
+#define TIMEOUT 50     // Time to wait for valid to be true, scaling appropriately for simulation time
 
 // Ready modes
 enum ReadyMode
@@ -26,6 +26,7 @@ const uint32_t RND_SEED = 1246504138;
 // Output counters
 int xCount = 0, yCount = 0, frameCount = 0;
 vluint64_t checkpoint = 0;
+bool valid, sof, eof;
 
 int main(int argc, char **argv, char **env)
 {
@@ -71,32 +72,105 @@ int main(int argc, char **argv, char **env)
     // main simulation loop
     for (vluint64_t simcyc = 0; simcyc < ENDTIME; ++simcyc)
     {
-        for (vluint64_t tick = 0; tick < 2; ++tick)
-        {
-            switch (READY_MODE)
-            {
-            case ALWAYS_READY:
-                ready = 1;
-                break;
-            case RANDOM_READY:
-                prbs = (prbs << 1) | (((prbs >> 32) ^ (~(prbs >> 19))) & 1);
-                ready = (prbs >> 32) & 1;
-                break;
-            case READY_AFTER_VALID:
-                if (top->out_stream_tvalid && ready)
-                    ready = 0;
-                else if (top->out_stream_tvalid)
-                    ready = 1;
-                else
-                    ready = 0;
-                break;
-            }
-            top->out_stream_tready = ready;
+        // Clock low
+        top->clk = 0;
+        top->eval();
+        tfp->dump(2 * simcyc);
+        // Clock high
+        top->clk = 1;
 
-            top->clk = !top->clk;
-            top->eval();
-            tfp->dump(2 * simcyc + tick);
+        // Ready signal generation
+        switch (READY_MODE)
+        {
+        case ALWAYS_READY:
+            ready = 1;
+            break;
+        case RANDOM_READY:
+            prbs = (prbs << 1) | (((prbs >> 32) ^ (~(prbs >> 19))) & 1);
+            ready = (prbs >> 32) & 1;
+            break;
+        case READY_AFTER_VALID:
+            if (top->out_stream_tvalid && ready)
+                ready = 0;
+            else if (top->out_stream_tvalid)
+                ready = 1;
+            else
+                ready = 0;
+            break;
         }
+        top->out_stream_tready = ready;
+
+        // evaluate the DUT so can update the outputs
+        top->eval();
+
+        // read output signals
+        valid = top->out_stream_tvalid;
+        sof = top->out_stream_tuser;
+        eol = top->out_stream_tlast;
+
+        // Check for timeout waiting for valid
+        if (valid)
+        {
+            checkpoint = simcyc;
+        }
+        if (simcyc > checkpoint + TIMEOUT)
+        {
+            std::cerr << "Error: Timeout waiting for valid" << std::endl;
+            checkpoint = simcyc;
+        }
+        if (valid && ready)
+        {
+
+            // Check for Start of Frame (tuser in AXI Stream) on first word of each frame
+            if (xCount == 0 && (yCount % Y_SIZE) == 0)
+            {
+                if (sof)
+                {
+                    std::cout << "SOF Ok on frame " << frameCount << std::endl;
+                    yCount = 0;
+                    frameCount++;
+                }
+                else
+                {
+                    std::cout << "Error: Expected SOF but not received" << std::endl;
+                }
+            }
+            else if (sof)
+            {
+                std::cout << "Error: Unexpected SOF received on word " << xCount << " of line " << yCount << " of frame " << frameCount << std::endl;
+                xCount = 0;
+                yCount = 0;
+                frameCount++;
+            }
+
+            // Check for End of Frame (tlast in AXI Stream) on last word of each frame
+            if (xCount == X_SIZE - 1)
+            {
+                if (eol)
+                {
+                    std::cout << "EOF Ok on line " << yCount << std::endl;
+                    xCount = 0;
+                    yCount++;
+                }
+                else
+                {
+                    std::cout << "Error: No EOL on word " << xCount - 1 << " of line " << yCount << std::endl;
+                    xCount++;
+                }
+            }
+            else if (eol)
+            {
+                std::cout << "Error: Unexpected EOL received on word " << xCount << " of line " << yCount << std::endl;
+                xCount = 0;
+                yCount++;
+            }
+            else
+            {
+                xCount++;
+            }
+        }
+
+        tfp->dump(2 * simcyc + 1);
     }
 
     tfp->close();
