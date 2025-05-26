7ec1c9b0a09fccd620ab56d28d592af8c245d3be
Ali Rabie <alirabie05@gmail.com>
2025-05-27 00:11:39 +0100

    top connection v2, zooming and panning are supported, they just need to be set in the testbench to generate the RGB values of each pixel. The logic with the start and ready signals may be simplified in a future iteration.

---
+++ top_connection v2/depth_calculator_LUT.sv
@@ -0,0 +1,22 @@
+module depth_calculator_LUT#(
+	// verilator lint_off UNUSED
+	parameter FRAC = 60,
+	parameter WORD_LENGTH = 64
+	// verilator lint_on UNUSED
+)(
+	input logic sysclk, start, reset,
+	// verilator lint_off UNUSED
+    input logic [10:0]        x,
+    input logic [10:0]        y,
+ 	// verilator lint_on UNUSED
+    input logic [WORD_LENGTH-1:0]       re_c,
+    input logic [WORD_LENGTH-1:0]       im_c,
+	output logic [23:0] color,
+	output logic done
+);
+logic [9:0]       final_depth;
+//logic             done;
+//logic [23:0]	color;
+depth_calculator #(.FRAC(FRAC),.WORD_LENGTH(WORD_LENGTH)) DC(.sysclk(sysclk),.start(start),.reset(reset),.x(x),.y(y),.re_c(re_c),.im_c(im_c),.final_depth(final_depth),.done(done));
+table_color TC(.depth(final_depth),.max_iterations(200),.clk(sysclk),.en(done), .color(color));
+endmodule
