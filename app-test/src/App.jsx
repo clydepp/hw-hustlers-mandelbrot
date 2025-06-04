9a71699069162ae62f345d249dd079f6967c5975
Clyde Pangilinan <pangiclyde@gmail.com>
2025-06-04 01:45:48 +0100

    Added functionality to dropdown, able to change max iterations using plus/minus buttons.

--- app-test/src/App.jsx
+++ app-test/src/App.jsx
@@ -1,18 +1,47 @@
 import { createSignal } from 'solid-js';
 import Button from './components/Button';
+import CascadeButton from './components/CascadeButton';
 
 function App() {
   const [isCollapsed, setIsCollapsed] = createSignal(false);
+  const [showNumbers, setShowNumbers] = createSignal(false);
+  const [counter, setCounter] = createSignal(1);
 
   const toggleCollapse = () => {
-    setIsCollapsed(!isCollapsed());
+    if (showNumbers()) {
+      // When showing numbers, act as -10 button
+      setCounter(counter() - 10);
+    } else {
+      // When showing icons, toggle collapse
+      setIsCollapsed(!isCollapsed());
+    }
+  };
+
+  const handlePlusClick = () => {
+    setShowNumbers(!showNumbers());
+  };
+
+  const handlePlusOne = () => {
+    setCounter(counter() + 1);
+  };
+
+  const handleMinusOne = () => {
+    setCounter(counter() - 1);
+  };
+
+  const handlePlusTen = () => {
+    setCounter(counter() + 10);
+  };
+
+  const handleMinusTen = () => {
+    setCounter(counter() - 10);
   };
 
   return (
     <div class="p-3">
       <div class="flex flex-col items-start gap-3">
         <Button onClick={toggleCollapse}>
-          {isCollapsed() ? "+" : "-"}
+          {showNumbers() ? "-10" : (isCollapsed() ? "+" : "-")}
         </Button>
         
         <div 
@@ -22,26 +51,31 @@ function App() {
             "opacity": isCollapsed() ? "0" : "1"
           }}
         >
-          <Button>
-            <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="m15 11.25 1.5 1.5.75-.75V8.758l2.276-.61a3 3 0 1 0-3.675-3.675l-.61 2.277H12l-.75.75 1.5 1.5M15 11.25l-8.47 8.47c-.34.34-.8.53-1.28.53s-.94.19-1.28.53l-.97.97-.75-.75.97-.97c.34-.34.53-.8.53-1.28s.19-.94.53-1.28L12.75 9M15 11.25 12.75 9" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
+          <CascadeButton 
+            showNumbers={showNumbers()} 
+            onMinusOne={handleMinusOne}
+          />
+          <Button onClick={handlePlusClick}>
+            {showNumbers() ? counter() : (
+              <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
+            )}
           </Button>
-          <Button>
-            <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
+          <Button onClick={handlePlusOne}>
+            {showNumbers() ? "+1" : (
+              <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" stroke-linecap="round" stroke-linejoin="round"></path>
+              <path d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
+            )}
           </Button>
-          <Button>
-            <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" stroke-linecap="round" stroke-linejoin="round"></path>
-            <path d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
-          </Button>
-          <Button>
-            <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-            <path d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z" stroke-linecap="round" stroke-linejoin="round"></path>
-            </svg>
+          <Button onClick={handlePlusTen}>
+            {showNumbers() ? "+10" : (
+              <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z" stroke-linecap="round" stroke-linejoin="round"></path>
+              </svg>
+            )}
           </Button>
         </div>
       </div>
---
+++ app-test/src/components/CascadeButton.jsx
@@ -0,0 +1,25 @@
+import Button from './Button.jsx'
+
+export default function CascadeButton (props){
+  const handleClick = () => {
+    if (props.showNumbers && props.onMinusOne) {
+      props.onMinusOne();
+    }
+  };
+
+  return (
+    <div class="grid grid-cols-5 gap-2">
+      <Button onClick={handleClick}>
+        {props.showNumbers ? "-1" : (
+          <svg width="20" height="20" data-slot="icon" aria-hidden="true" fill="none" stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+          <path d="m15 11.25 1.5 1.5.75-.75V8.758l2.276-.61a3 3 0 1 0-3.675-3.675l-.61 2.277H12l-.75.75 1.5 1.5M15 11.25l-8.47 8.47c-.34.34-.8.53-1.28.53s-.94.19-1.28.53l-.97.97-.75-.75.97-.97c.34-.34.53-.8.53-1.28s.19-.94.53-1.28L12.75 9M15 11.25 12.75 9" stroke-linecap="round" stroke-linejoin="round"></path>
+          </svg>
+        )}
+      </Button>
+      <Button>GS</Button>
+      <Button>CL</Button>
+      <Button>FIRE</Button>
+      <Button>OCE</Button>
+    </div>
+  );
+}
